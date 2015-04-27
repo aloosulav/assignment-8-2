7aeaaa0ce876239f5f057b14b77e5cc09596334d
aloosulav <acharyasulav@hotmail.com>
2015-04-27 14:14:34 -0400

    fileinput

--- cycle/src/cycle/CycleDriver.java
+++ cycle/src/cycle/CycleDriver.java
@@ -1,21 +1,35 @@
 package cycle;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+
 import java.util.Scanner;
 
 public class CycleDriver  {
 	
-		    public static void main(String args[]) throws IOException{
+		    public static void main(String args[]) throws NullPointerException{
 		    	 
 		    		  
-		    	
-		          Scanner key = new Scanner(System.in);
-		          System.out.println("Enter Number of Wheels of Cycle:");
-		         int numberOfWheels = key.nextInt();
-		         
+		    	Scanner inputFile=null;
+		         String fileName= "Cycle.txt";
+		         try{
+		         inputFile= new Scanner(new File(fileName));
+		    	}
+		         catch(Exception e){
+		        	 System.out.println("File not found");
+		         }
+		          //System.out.println("Enter Number of Wheels of Cycle:");
+		     String[]name=new String[2];
+		     int i=0;
+		         while(inputFile.hasNextLine()){
+		         name[i]=inputFile.nextLine();
+		         i++;
+		         }
+		         inputFile.close();
+		         for (String n:name){
+		        	 System.out.println(n);
+		         }
+		         /*
 		          System.out.println("Enter Weight of Cycle :");
 		          int weight = key.nextInt();
 		         
@@ -32,7 +46,7 @@ public class CycleDriver  {
 		           PW.println("Weight "+C.getWeight());
 		      
 		           PW.close();
-		           
+		         */
 		    }
 		    }
 		    
